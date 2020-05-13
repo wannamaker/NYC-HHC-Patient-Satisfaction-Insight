55765795f8b2e2d3cab222671d7be1b4bbe81c6c
Wannamaker <wannamaker.charles@outlook.com>
2020-05-13 09:52:01 -0400

    realized 2 main functions

--- script.js
+++ script.js
@@ -1,28 +1,7 @@
 
 
-const getHospital = async () => {
 
-  const url = "https://data.cityofnewyork.us/resource/hi3x-y76v.json";
-
-  try {
-
-    const response = await axios.get(url);
-    console.log(response.data.length);
-    console.log(response.data);
-    console.log(response);
-
-    dataArray = response.data;
-
-    console.log(dataArray[0]['hospital_name']);
-
-
-  } catch (error) {
-    console.log(`${error}`);
-  }
-
-}
-
-hospitalName = [
+let hospitalName = [
   "BELLEVUE HOSPITAL CENTER",
   "CONEY ISLAND HOSPITAL",
   "ELMHURST HOSPITAL CENTER",
@@ -36,6 +15,39 @@ hospitalName = [
   "WOODHULL MEDICAL AND MENTAL HEALTH CENTER"
 ]
 
-getHospital();
 
-console.log(hospitalName.length);
+const getData = async () => {
+
+  const url = "https://data.cityofnewyork.us/resource/hi3x-y76v.json";
+
+  try {
+
+    const response = await axios.get(url);
+
+    let dataArray = response.data;
+
+  } catch (error) {
+    console.log(`${error}`);
+  }
+  return dataArray;
+}
+
+getData();
+
+
+function survey(arr, str) {
+  let hospitalObj = {};
+  let hospArrayObj = [];
+  let hospArray1 = [];
+  let hospitalSurvey = [];
+  let hospital = str;
+  for (let j = 0; j < 110; j++) {
+    for (let k in arr[j]) {
+      let buffer;
+      if ((k === 'hospital_name') && (arr[j].hospital_name === hospital)) {
+        hospArray1.push(dataArray[j]);
+      }
+    }
+  } hospitalObj[hospital] = hospArray1;
+  return hospitalObj;
+};
